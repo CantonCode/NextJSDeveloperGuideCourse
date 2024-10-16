042eb8cccbe963f89b810e2b07d18e8729898b85
Conor Canton <conorstreete@gmail.com>
2024-10-17 11:02:04 +1300

    intro to server actions

--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,6 +1,30 @@
+import { db } from '@/app/db';
+import { redirect } from 'next/navigation';
+
 export default function SnippetCreatePage() {
+    async function createSnippet(formData: FormData) {
+        //server action
+        'use server';
+
+        //check users input and validate
+        const title = formData.get('title') as string;
+        const code = formData.get('code') as string;
+
+        //create new record in DB
+        const snippet = await db.snippet.create({
+            data:{
+                title,
+                code
+            },
+        });
+
+        console.log(snippet)
+
+        //redirect to home
+        redirect('/');
+    }
     return (
-        <form>
+        <form action={createSnippet}>
             <h3 className="font-bold m-3"> Create a Snippet</h3>
             <div className="flex flex-col gap-4">
                 <div className="flex gap-4">
