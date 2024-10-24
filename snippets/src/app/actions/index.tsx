7a0086f3de339f05558d94b9169efbed9ad6fb99
Conor Canton <conorstreete@gmail.com>
2024-10-24 14:29:44 +1300

    completed edit funcitonality

--- snippets/src/app/actions/index.tsx
+++ snippets/src/app/actions/index.tsx
@@ -1,5 +1,13 @@
 'use server'
 
-export async function editSnippet() {
-    console.log("editing snippet log")
+import { redirect } from "next/navigation";
+import { db } from "../db"
+
+export async function editSnippet(id:number,code:string) {
+    await db.snippet.update({
+        where:{id},
+        data:{code}
+    });
+
+    redirect(`/snippets/${id}`)
 }
